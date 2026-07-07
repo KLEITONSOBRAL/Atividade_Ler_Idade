d9f0a12b382cc55856db6c3e7b62ed3088cee7d7
Kleiton Sobral <kleiton.c@aluno.senai.br>
2026-07-07 11:17:19 -0300

    Update PERGUNTA_2.java
    Atualização.

--- PERGUNTA_2/src/main/java/com/mycompany/pergunta_2/PERGUNTA_2.java
+++ PERGUNTA_2/src/main/java/com/mycompany/pergunta_2/PERGUNTA_2.java
@@ -1,8 +1,8 @@
-/*
- * Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e mostre-a expressa em dias.
-Leve em consideração o ano com 365 dias e o mês com 30.
+/* Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e mostre-a expressa em dias.
+   Leve em consideração o ano com 365 dias e o mês com 30 dias.
 
-(Ex: 3 anos, 2 meses e 15 dias = 1170 dias.) 
+   (Ex: 3 anos, 2 meses e 15 dias = 1170 dias.) 
+   
  */
 
 package com.mycompany.pergunta_2;
@@ -13,18 +13,23 @@ public class PERGUNTA_2 {
 
     public static void main(String[] args) {
         
-        int dias, anos, meses;
+        int meses = 0, dias = 0, anos = 0 , totalDias = 0;
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite a sua idade em números:");
-        int idade = scanner.nextInt();
+        System.out.println("Digite os anos:");
+        anos = scanner.nextInt();
 
-        dias = idade * 365;
-        meses = idade / 12;
+        System.out.println("Digite os meses:");
+        meses = scanner.nextInt();
 
-        System.out.println("Você tem "+ idade + " anos " + meses + " meses " + dias + " dias");
-        
-        
+        System.out.println("Digite os dias:");
+        dias = scanner.nextInt();
+
+        totalDias = (anos * 365) + (meses * 30) + dias;
+
+        System.out.println(anos + " anos, " + meses + " meses e " + dias + " dias equivalem a " + totalDias +" total de dias.");
+
+               
     }
 }
